fcbb0be5bfe3e637e7bb80da2659cef0c9afa967
OP (oppenheimer) <21008429+Ompragash@users.noreply.github.com>
2024-11-27 12:58:57 +0530

    Update pkg/output/output.go

--- pkg/output/output.go
+++ pkg/output/output.go
@@ -15,9 +15,6 @@ func WritePluginOutputFile(outputFilePath, digest string, pluginTarPath string) 
 		output["IMAGE_TAR_PATH"] = pluginTarPath
 	}
 
-	if len(output) == 0 {
-		return fmt.Errorf("no values to write to output file")
-	}
 
 	return godotenv.Write(output, outputFilePath)
 }
